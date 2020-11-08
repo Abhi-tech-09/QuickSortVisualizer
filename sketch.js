7ad666bd1ff2714ad91bc2b0ce125855a9357019
Abhi-tech-09 <abhishekalien09@gmail.com>
2020-11-08 20:35:29 +0530

    Added Media Queries and new Variables.

--- sketch.js
+++ sketch.js
@@ -1,93 +1,93 @@
-var w = window.innerWidth, h = window.innerHeight,
-    toolsHeight = document.querySelector('.tools').offsetHeight,
-	n = 0, a = [], flag = [], cur = -1, it1 = -1, it2 = -1, isCustom, speed = 700,	
-    counter = document.querySelector('#counter');
+var w = window.innerWidth,
+	h = window.innerHeight,
+	toolsHeight = document.querySelector('.tools').offsetHeight,
+	smHeight = document.querySelector('#sm-screen').offsetHeight,
+	n = 0,
+	a = [],
+	flag = [],
+	cur = -1,
+	it1 = -1,
+	it2 = -1,
+	isCustom,
+	speed = 700,
+	counter = document.querySelector('#counter');
 
 const genBtn = document.querySelector('#genArr');
-const shuffleBtn = document.querySelector('#shuffleBtn');
+const shuffleBtn = document.querySelectorAll('.shuffleBtn');
 const customInput = document.querySelector('#customEleIpt');
+const visQuickSort = document.querySelector('#visualBtn	');
+const navVisQuickSort = document.querySelector('#navVisualBtn');
 
 function setup() {
-	let canvas = createCanvas(w, h - toolsHeight);
+	let canvas = createCanvas(w, h - toolsHeight / 2.2 + smHeight / 20);
+	canvas.parent('canvas');
 	canvas.style('display', 'block');
 
-	let visQuickSort = document.querySelector('#visualBtn');
+	visQuickSort.addEventListener('click', function checker() {
+		if (isCustom) {
+			a = customInput.value.split(',').map((e) => parseInt(e));
+		}
 
-    visQuickSort.addEventListener('click', function checker() {
-        if (isCustom) {
-            a = customInput.value.split(",").map(e => parseInt(e));
-        }
-        
-        flag = new Array(a.length);
-        flag.fill(0);
-        quickSort(a, 0, a.length - 1);
-        
+		flag = new Array(a.length);
+		flag.fill(0);
+		quickSort(a, 0, a.length - 1);
+	});
+
+	navVisQuickSort.addEventListener('click', function checker() {
+		if (isCustom) {
+			a = customInput.value.split(',').map((e) => parseInt(e));
+		}
+
+		flag = new Array(a.length);
+		flag.fill(0);
+		quickSort(a, 0, a.length - 1);
 	});
 }
 
 function generate() {
 	let rdSize = document.querySelector('#randomSzIpt').value;
 	a = [];
-    n = parseInt(rdSize);
+	n = parseInt(rdSize);
 
-    if (!n) n = Math.round(random(20, 100));
-    
-    a = new Array(n);    
+	if (!n) n = Math.round(random(20, 100));
+
+	a = new Array(n);
 	for (let i = 0; i < a.length; ++i) a[i] = Math.round(random(50, 500));
 	flag = new Array(a.length);
-    flag.fill(0);
-    it1 = it2 = cur = -1;
+	flag.fill(0);
+	it1 = it2 = cur = -1;
 }
 
 function isSorted(arr) {
 	let temp = true;
 	for (let i = 1; i < arr.length; ++i) {
 		if (arr[i - 1] > arr[i]) {
-            temp = false;
+			temp = false;
 			break;
 		}
 	}
 	return temp;
 }
 
-shuffleBtn.addEventListener('click', () => {
-	let temp = [], val, len = a.length;
-
-	while (temp.length < len) {
-		let idx = Math.round(random(0, a.length - 1));
-		val = a[idx];
-		a.splice(idx, 1);
-		temp.push(val);
-	}
-
-	if (isSorted(temp)) {
-        let idx1 = Math.round(random(0, a.length / 2)), idx2 = Math.round(random((a.length + 1) / 2, a.length + 1));
-		[ temp[idx1], temp[idx2] ] = [ temp[idx2], temp[idx1] ];
-	}
-
-	a = temp;
-    flag.fill(0);
-    it1 = it2 = cur = -1;
-});
-
 function draw() {
 	background('#0F2027');
-    textAlign(CENTER);
-    checkCustom();
-    
+	textAlign(CENTER);
+	checkCustom();
+
 	if (a.length > 0) {
-		if (flag[0]!=2) {
-			if(cur >=0) flag[cur] = 1;
+		if (flag[0] != 2) {
+			if (cur >= 0) flag[cur] = 1;
 			if (it1 >= 0) flag[it1] = flag[it2] = 3;
 		}
 
-		let hRatio = Math.round(w / a.length), x = 0;
+		let hRatio = Math.round(w / a.length),
+			x = 0;
 		let mul = w % a.length;
 		mul /= a.length;
 		hRatio += mul;
-		let mx1 = h - toolsHeight - 10;
+		let mx1 = h - toolsHeight - smHeight - 10;
 		let mx2 = Math.max(...a);
-		let factor = mx1/mx2;
+		let factor = mx1 / mx2;
 
 		for (let i = 0; i < a.length; ++i) {
 			if (flag[i] == 3) fill('#3b5bff');
@@ -95,19 +95,45 @@ function draw() {
 			else if (flag[i] == 1) fill('#F00000');
 			else fill(255);
 
-			rect(x, h - (a[i]*factor) - toolsHeight, hRatio, a[i]*factor);
+			strokeWeight(1);
+			rect(x, h - a[i] * factor - toolsHeight / 2.2 - smHeight / 20, hRatio, a[i] * factor);
 			x += hRatio;
 		}
 
-		if (flag[0]!=2) {
-			if(cur >=0) flag[cur] = 0;
+		if (flag[0] != 2) {
+			if (cur >= 0) flag[cur] = 0;
 			if (it1 >= 0) flag[it1] = flag[it2] = 0;
 		}
-    }
+	}
+	
+	for (let i = 0; i < 2; ++i) {
+		shuffleBtn[i].addEventListener('click', () => {
+			let temp = [],
+				val,
+				len = a.length;
+
+			while (temp.length < len) {
+				let idx = Math.round(random(0, a.length - 1));
+				val = a[idx];
+				a.splice(idx, 1);
+				temp.push(val);
+			}
+
+			if (isSorted(temp)) {
+				let idx1 = Math.round(random(0, a.length / 2)),
+					idx2 = Math.round(random((a.length + 1) / 2, a.length + 1));
+				[ temp[idx1], temp[idx2] ] = [ temp[idx2], temp[idx1] ];
+			}
+
+			a = temp;
+			flag.fill(0);
+			it1 = it2 = cur = -1;
+		});
+	}
 }
 
 function checkCustom() {
-    isCustom = genBtn.disabled = (customInput.value.trim().length != 0);
+	isCustom = genBtn.disabled = customInput.value.trim().length != 0;
 }
 
 async function quickSort(arr, low, high) {
@@ -116,7 +142,7 @@ async function quickSort(arr, low, high) {
 		Promise.all([ quickSort(arr, low, idx - 1), quickSort(arr, idx + 1, high) ]);
 	}
 
-    if (isSorted(a)) flag.fill(2);
+	if (isSorted(a)) flag.fill(2);
 }
 
 async function partition(arr, low, high) {
@@ -148,25 +174,22 @@ function sleep(ms) {
 function windowResized() {
 	w = window.innerWidth;
 	h = window.innerHeight;
-	resizeCanvas(w, h);
+	resizeCanvas(w, h - toolsHeight / 2.2 + smHeight / 20);
 }
 
-function changeSpeed1(){ 
-	if(speed<1000)
-		speed+=100;
-	else
-		speed+=1000;
-		counter.innerText = speed;
-}
-function changeSpeed2(){ 
-	speed = 700; 
+function changeSpeed1() {
+	if (speed < 1000) speed += 100;
+	else speed += 1000;
 	counter.innerText = speed;
 }
-function changeSpeed3(){ 
-	if(speed>1000)
-		speed-=1000;
-	else if(speed>100)
-		speed-=100;
+function changeSpeed2() {
+	speed = 700;
 	counter.innerText = speed;
-	
+}
+function changeSpeed3() {
+	if (speed > 1000) speed -= 1000;
+	else if (speed > 100) speed -= 100;
+	else if (speed <= 100 && speed >= 10) speed -= 10;
+
+	counter.innerText = speed;
 }
